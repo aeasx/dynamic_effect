325f73abcfdf70fcd11dc09bc3f02036827e62c7
肖振洋 <3089326386@qq.com>
2025-09-04 20:05:13 +0800

    避免重复的state

--- src/components/StateControl/Menu.tsx
+++ src/components/StateControl/Menu.tsx
@@ -13,12 +13,10 @@ const initialItems: Item[] = [
 ]
 
 export function Menu() {
-  // 这样并不好，selectedItem 的内容与items列表中的某一项是同一个对象。
-  // 这意味着关于该项本身的信息 在2个地方产生了重复
   const [items, setItems] = useState<Item[]>(initialItems)
-  const [selectedItem, setSelectedItem] = useState<Item>(
-    items[0]
-  )
+  const [selectedId, setSelectedId] = useState(0)
+  const selectedItem = items.find(f => f.id === selectedId)!
+
   function handleItemChange(id: number, e: ChangeEvent<HTMLInputElement>) {
     setItems(items.map(item => {
       if (item.id === id) {
@@ -30,9 +28,8 @@ export function Menu() {
         return item
       }
     }))
-    // const selectedItemNow = items.find(f => f.id === id)!
-    // setSelectedItem(selectedItemNow)
   }
+
   return (
     <>
       <h2>What's your travel snack?</h2>
@@ -42,9 +39,7 @@ export function Menu() {
             <Input style={{ maxWidth: 140 }} value={item.title} allowClear onChange={e => handleItemChange(item.id, e)}></Input>
             {item.title}
             {' '}
-            <Button type='primary' onClick={() => {
-              setSelectedItem(item)
-            }}>Choose</Button>
+            <Button type='primary' onClick={() => setSelectedId(item.id)}>Choose</Button>
           </li>
         ))}
       </ul>
@@ -52,8 +47,3 @@ export function Menu() {
     </>
   )
 }
-/* -------------------------------------------------------------------------- */
-// 上面的代码存在问题
-// 1.如果首先点击了 Choose 然后编辑它，输入会更新，但是底部的标签不会反映编辑内容。
-// 这是因为有重复的 state ，并且忘记更新了 state。尽管我可以更新 selectedItem
-// 但是更简单的解决办法是消除重复项。
